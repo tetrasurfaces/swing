cb16cc1359456baf57014283b93b17817723257e
tetrasurfaces <sendtotoddhutchinson@gmail.com>
2026-03-17 12:19:09 +0900

    Update swing.py

--- swing.py
+++ swing.py
@@ -1,5 +1,5 @@
 # Dual License:
-# - For core software: AGPL-3.0-or-later licensed. -- xAI fork, 2025
+# - For core software: AGPL-3.0-or-later licensed.
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
