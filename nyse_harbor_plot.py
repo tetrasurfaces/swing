29c97adab0fa185b5ff0dc77c64b7d8a4d18280e
tetrasurfaces <sendtotoddhutchinson@gmail.com>
2026-04-24 14:39:38 +0930

    Update nyse_harbor_plot.py

--- nyse_harbor_plot.py
+++ nyse_harbor_plot.py
@@ -1,20 +1,15 @@
-# - For core software: AGPL-3.0-or-later licensed.
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# PRIVATE AND CONFIDENTIAL
+# Copyright (c) 2025 Tetrasurfaces. All Rights Reserved.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
+# This file and all associated intellectual property (including harbor lines,
+# nested sailfish pattern recognition, swing chain logic, multi-timeframe
+# reversal systems, and trading models) are strictly confidential.
 #
-# You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# Unauthorized distribution, disclosure, copying, or use is prohibited.
+# Mutual Non-Disclosure Agreement applies to all collaborators.
 #
-# Copyright 2025 Tetrasurfaces
-
-# development in progress
+# For internal backup and development use only.
+# Last updated: 2026-04-23
 
 #!/usr/bin/env python3
 """
